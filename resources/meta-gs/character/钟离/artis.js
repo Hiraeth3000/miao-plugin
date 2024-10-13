c6c516b710370530b7d064c5e53bf88f0a1bab44
青衣悠然 <c.month@foxmail.com>
2024-10-13 08:59:22 +0800

    Add files via upload

--- resources/meta-gs/character/钟离/artis.js
+++ resources/meta-gs/character/钟离/artis.js
@@ -1,11 +1,13 @@
 import { usefulAttr } from "../../artifact/artis-mark.js"
 
-export default function ({ attr, weapon, rule, def }) {
+export default function ({ attr, weapon, artis, rule, def }) {
   let title = []
   let particularAttr = JSON.parse(JSON.stringify(usefulAttr['钟离']))
   if (weapon.name === '西风长枪') {
     title.push('西风枪')
     particularAttr.cpct = 100
+    particularAttr.dmg = 50
+    particularAttr.recharge = 80
   }
   if (attr.cpct * 2 + attr.cdmg > 240) {
     title.push('战斗')
@@ -15,6 +17,11 @@ export default function ({ attr, weapon, rule, def }) {
     particularAttr.cdmg = 100
     particularAttr.dmg = 100
     particularAttr.recharge = 30
+  }
+  if (artis.is('绝缘4')) {
+    title.push('绝缘4')
+    particularAttr.dmg = 80
+    particularAttr.recharge = 80
   }
   if (title.length > 0) {
     return rule(`钟离-${title.join('')}`, particularAttr)
