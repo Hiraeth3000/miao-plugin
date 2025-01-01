0329a75936686ff8d7b693094879aba613c9fa5d
青衣悠然 <c.month@foxmail.com>
2025-01-01 23:20:42 +0800

    Update artis.js

--- resources/meta-gs/character/闲云/artis.js
+++ resources/meta-gs/character/闲云/artis.js
@@ -1,7 +1,7 @@
 import { usefulAttr } from "../../artifact/artis-mark.js"
 
-export default function ({ cons, rule, def }) {
-    if (cons === 6) {
+export default function ({ attr, rule, def }) {
+  if (attr.cpct * 2 + attr.cdmg > 240) {
       return rule('闲云-满命', { hp: 0, atk: 100, def: 0, cpct: 100, cdmg: 100, mastery: 0, dmg: 100, phy: 0, recharge: 35, heal: 95 })
     }
     return def(usefulAttr['闲云'])
